9cc8c039aa2369fd8988ea9bd8c769cf6d36fb93
Maksymilian Majek <majek.maks@wp.pl>
2025-04-27 18:22:27 +0200

    init

--- Django/djangotutorial/polls/tests.py
+++ Django/djangotutorial/polls/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from .models import Question, Choice
+from django.utils import timezone
+
+class VotingTest(TestCase):
+    def test_vote_on_question(self):
+        question = Question.objects.create(question_text="What's new?", pub_date=timezone.now())
+        choice = Choice.objects.create(question=question, choice_text="Not much", votes=0)
+
+        response = self.client.post(reverse('polls:vote', args=(question.id,)), {'choice': choice.id})
+
+        self.assertRedirects(response, reverse('polls:results', args=(question.id,)))
--- Django/djangotutorial/polls/urls.py
+++ Django/djangotutorial/polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'polls'  # Dodaj tę linię
+app_name = 'polls' 
 
 urlpatterns = [
     path('', views.index, name='index'),
