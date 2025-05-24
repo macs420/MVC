3a9a816add96cea07aa0d3034028bd624ecb944e
maksmajek <majek.maks@wp.pl>
2025-05-24 17:19:25 +0200

    init final

--- .NET/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ .NET/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cb8a2bef5b4dd643f07caafc44ed53521f6f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0fd9168501067d5d418e03ca567c32f1c8192d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
